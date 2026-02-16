1806c0027e37306a4bafab02c3decf9da91b76b8
profGiovanniVolpintesta <giovanni.volpintesta@itisriva.edu.it>
2026-02-16 11:47:08 +0100

    Output menu utente
    Output menu utente

--- Program.cs
+++ Program.cs
@@ -10,7 +10,20 @@ namespace AgendaPersonale
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("GOOGLE CALENDAR NON PLUS ULTRA");
 
+            stampaMenu();
+
+        }
+
+        static void stampaMenu()
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("1 - Visualizza le attività");
+            Console.WriteLine("2 - Inserisci attività");
+            Console.WriteLine("3 - Modifica attività");
+            Console.WriteLine("4 - Elimina attività");
+            Console.WriteLine("0 - ESCI");
         }
     }
 }
